ef276a9e180aec845d62f8590bf18d88d8c4243e
TrixiS <oficialmorozov@gmail.com>
2020-04-23 18:52:53 +0500

    Add base template and static

---
+++ static/js/index.js
@@ -0,0 +1,6 @@
+
+function inviteLink() {
+    let url = "https://discordapp.com/oauth2/authorize?&client_id=494573728454279178&scope=bot&permissions=8";
+    open(url, '', 'top=50, left=325, width=350, height=600, status=no, scrollbars=yes');
+    return false;
+}
