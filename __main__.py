f4220ad5a01c84985103e2e49a94f4c155f47360
TrixiS <oficialmorozov@gmail.com>
2020-04-23 18:52:06 +0500

    Improve language system

--- __main__.py
+++ __main__.py
@@ -21,6 +21,14 @@ def get_default_language():
     return "en"
 
 
+def render_with_lang(*args, **kwargs):
+    name = args[0].split('.')[0]
+    kwargs["lang"] = app.langs[current_language]
+    kwargs["langs"] = tuple(l for l in app.langs.keys() if l != kwargs["lang"]["lang_code"])
+    kwargs["title"] = kwargs["lang"][name]
+    return render_template(*args, **kwargs)
+
+
 @app.route("/setlang/")
 def setlang():
     lang.change_language(request.args.get("new_lang") or "en")
@@ -29,7 +37,7 @@ def setlang():
 
 @app.route('/')
 def index():
-    return "TODO: home"
+    return render_with_lang("index.html")
 
 
 app.secret_key = Config.secret_key
