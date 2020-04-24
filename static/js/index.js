865a1ee2913e0e1bafcebb81a69a0a381c6e9256
TrixiS <oficialmorozov@gmail.com>
2020-04-24 22:08:30 +0500

    Move urls to the separate consts

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,12 @@
+const botInviteUrl = "https://discordapp.com/oauth2/authorize?&client_id=494573728454279178&scope=bot&permissions=8";
+const supportInviteUrl = "https://discord.gg/VjsUafD";
 
 function inviteLink() {
-    let url = "https://discordapp.com/oauth2/authorize?&client_id=494573728454279178&scope=bot&permissions=8";
-    open(url, '', 'top=50, left=325, width=350, height=600, status=no, scrollbars=yes');
+    open(botInviteUrl, '', 'top=50, left=325, width=350, height=600, status=no, scrollbars=yes');
     return false;
 }
 
 function supportLink() {
-    let url = "https://discord.gg/VjsUafD";
-    open(url, '', 'top=50, left=325, width=350, height=600, status=no, scrollbars=yes');
+    open(supportInviteUrl, '', 'top=50, left=325, width=350, height=600, status=no, scrollbars=yes');
     return false;
 }
