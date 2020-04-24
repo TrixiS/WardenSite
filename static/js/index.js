e51c4b25ee0b13f8324a9043c51580670ba6bc32
TrixiS <oficialmorozov@gmail.com>
2020-04-24 22:11:26 +0500

    Move window args to the single const

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,13 @@
 const botInviteUrl = "https://discordapp.com/oauth2/authorize?&client_id=494573728454279178&scope=bot&permissions=8";
 const supportInviteUrl = "https://discord.gg/VjsUafD";
+const windowArgs = "top=50, left=325, width=350, height=600, status=no, scrollbars=yes";
 
 function inviteLink() {
-    open(botInviteUrl, '', 'top=50, left=325, width=350, height=600, status=no, scrollbars=yes');
+    open(botInviteUrl, '', windowArgs);
     return false;
 }
 
 function supportLink() {
-    open(supportInviteUrl, '', 'top=50, left=325, width=350, height=600, status=no, scrollbars=yes');
+    open(supportInviteUrl, '', windowArgs);
     return false;
 }
